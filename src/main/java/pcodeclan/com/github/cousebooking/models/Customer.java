84147d451e3d91d52e043c69847db697a03a2c78
User <user@Users-MacBook-Pro.local>
2020-06-11 15:39:12 +0100

    created customer model
    Signed-off-by: User <user@Users-MacBook-Pro.local>

---
+++ src/main/java/pcodeclan/com/github/cousebooking/models/Customer.java
@@ -0,0 +1,80 @@
+package pcodeclan.com.github.cousebooking.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // name, city, age
+    @Column
+    private String name;
+
+    @Column
+    private String city;
+
+    @Column
+    private int age;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    public Customer(String name, String city, int age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
