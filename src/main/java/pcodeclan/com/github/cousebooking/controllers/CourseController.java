f874e358bcce0013b2f9615819fd280d79737a3d
User <user@Users-MacBook-Pro.local>
2020-06-11 19:31:27 +0100

    get all courses with a given rating controller done
    Signed-off-by: User <user@Users-MacBook-Pro.local>

--- src/main/java/pcodeclan/com/github/cousebooking/controllers/CourseController.java
+++ src/main/java/pcodeclan/com/github/cousebooking/controllers/CourseController.java
@@ -3,36 +3,34 @@ package pcodeclan.com.github.cousebooking.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pcodeclan.com.github.cousebooking.models.Course;
 import pcodeclan.com.github.cousebooking.repositories.CourseRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/courses")
 public class CourseController {
 
     @Autowired
     private CourseRepository courseRepository;
 
     //Get course by ID
-    @GetMapping
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Course> getOneCourse(@PathVariable Long id){
         return new ResponseEntity<>(courseRepository.findById(id).get(), HttpStatus.OK);
     }
 
     //Get all courses with a given rating
-//    @GetMapping
-//    public ResponseEntity<List<Course>> getAllCoursesByRating(
-//            @RequestParam(name="rating", required = false) Integer rating
-//    ){
-//        if (rating !=null){
-//            return new ResponseEntity<>(courseRepository.findByStarRating(rating), HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
-//    }
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCoursesByRating(
+            @RequestParam(name="rating", required = false) Integer rating
+    ){
+        if (rating !=null){
+            return new ResponseEntity<>(courseRepository.findByStarRating(rating), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
 
 }
