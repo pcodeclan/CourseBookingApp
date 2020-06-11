e29b2ab474462f1296e30f8ff8363ad169ba42b9
User <user@Users-MacBook-Pro.local>
2020-06-11 15:42:17 +0100

    added customer to dataloader
    Signed-off-by: User <user@Users-MacBook-Pro.local>

--- src/main/java/pcodeclan/com/github/cousebooking/components/DataLoader.java
+++ src/main/java/pcodeclan/com/github/cousebooking/components/DataLoader.java
@@ -6,8 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import pcodeclan.com.github.cousebooking.models.Booking;
 import pcodeclan.com.github.cousebooking.models.Course;
+import pcodeclan.com.github.cousebooking.models.Customer;
 import pcodeclan.com.github.cousebooking.repositories.BookingRepository;
 import pcodeclan.com.github.cousebooking.repositories.CourseRepository;
+import pcodeclan.com.github.cousebooking.repositories.CustomerRepository;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -18,6 +20,9 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     BookingRepository bookingRepository;
 
+    @Autowired
+    CustomerRepository customerRepository;
+
     public DataLoader() {
     }
 
@@ -27,6 +32,9 @@ public class DataLoader implements ApplicationRunner {
 
         Booking booking1 = new Booking("01-05-20", course1);
         bookingRepository.save(booking1);
+
+        Customer customer1 = new Customer("Peter", "East Kilbride", 27);
+        customerRepository.save(customer1);
 
 
     }
